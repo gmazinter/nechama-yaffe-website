9a4407ad342c459590eaf087d9333a121137cc8f
Guy Mazinter <gmazinter@gmail.com>
2019-05-19 12:56:57 +0300

    initial commit

---
+++ paintingsData.js
@@ -0,0 +1,122 @@
+const paintings = [
+    {
+        fileName: "painting20.jpg",
+        size: "30cm X 45cm",
+        technique: "acrylic on canvas",
+        year: 2015
+    },
+    {
+        fileName: "painting19.jpg",
+        size: "30cm X 45cm",
+        technique: "acrylic on canvas",
+        year: 2015
+    },
+    {
+        fileName: "painting18.jpg",
+        size: "30cm X 45cm",
+        technique: "acrylic on canvas",
+        year: 2015
+    },
+    {
+        fileName: "painting17.jpg",
+        size: "30cm X 45cm",
+        technique: "acrylic on canvas",
+        year: 2015
+    },
+    {
+        fileName: "painting16.jpg",
+        size: "30cm X 45cm",
+        technique: "acrylic on canvas",
+        year: 2015
+    },
+    {
+        fileName: "painting15.jpg",
+        size: "30cm X 45cm",
+        technique: "acrylic on canvas",
+        year: 2015
+    },
+    {
+        fileName: "painting14.jpg",
+        size: "30cm X 45cm",
+        technique: "acrylic on canvas",
+        year: 2015
+    },
+    {
+        fileName: "painting13.jpg",
+        size: "30cm X 45cm",
+        technique: "acrylic on canvas",
+        year: 2015
+    },
+    {
+        fileName: "painting12.jpg",
+        size: "30cm X 45cm",
+        technique: "acrylic on canvas",
+        year: 2015
+    },
+    {
+        fileName: "painting11.jpg",
+        size: "30cm X 45cm",
+        technique: "acrylic on canvas",
+        year: 2015
+    },
+    {
+        fileName: "painting10.jpg",
+        size: "30cm X 45cm",
+        technique: "acrylic on canvas",
+        year: 2015
+    },
+    {
+        fileName: "painting9.jpg",
+        size: "30cm X 45cm",
+        technique: "acrylic on canvas",
+        year: 2015
+    },
+    {
+        fileName: "painting8.jpg",
+        size: "30cm X 45cm",
+        technique: "acrylic on canvas",
+        year: 2015
+    },
+    {
+        fileName: "painting7.jpg",
+        size: "30cm X 45cm",
+        technique: "acrylic on canvas",
+        year: 2015
+    },
+    {
+        fileName: "painting6.jpg",
+        size: "30cm X 45cm",
+        technique: "acrylic on canvas",
+        year: 2015
+    },
+    {
+        fileName: "painting5.jpg",
+        size: "30cm X 45cm",
+        technique: "acrylic on canvas",
+        year: 2015
+    },
+    {
+        fileName: "painting4.jpg",
+        size: "30cm X 45cm",
+        technique: "acrylic on canvas",
+        year: 2015
+    },
+    {
+        fileName: "painting3.jpg",
+        size: "30cm X 45cm",
+        technique: "acrylic on canvas",
+        year: 2015
+    },
+    {
+        fileName: "painting2.jpg",
+        size: "30cm X 45cm",
+        technique: "acrylic on canvas",
+        year: 2015
+    },
+    {
+        fileName: "painting1.jpg",
+        size: "30cm X 45cm",
+        technique: "acrylic on canvas",
+        year: 2015
+    }
+]
